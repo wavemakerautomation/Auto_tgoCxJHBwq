21fa96561dadd9708baea321e796589e62b97072
wave test <automate9@wavemaker.com>
2017-11-30 22:25:38 +0000

    Auto commit Files modified - [securityService,profiles,webapp,project-security.xml]

--- src/main/webapp/wmProperties.js
+++ src/main/webapp/wmProperties.js
@@ -6,7 +6,7 @@ var _WM_APP_PROPERTIES = {
   "homePage" : "Main",
   "name" : "Auto_tgoCxJHBwq",
   "platformType" : "WEB",
-  "securityEnabled" : "false",
+  "securityEnabled" : "true",
   "supportedLanguages" : "en",
   "timeFormat" : "",
   "type" : "APPLICATION",
