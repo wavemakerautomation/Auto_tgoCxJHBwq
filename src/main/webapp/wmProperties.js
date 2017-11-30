a3609715057fbb3728fb9789b03d7e9599861358
wave test <automate9@wavemaker.com>
2017-11-30 22:22:55 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_tgoCxJHBwq",
+  "homePage" : "Main",
+  "name" : "Auto_tgoCxJHBwq",
+  "platformType" : "WEB",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
